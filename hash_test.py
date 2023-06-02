f8624328dffe10492534d4b7d60fbe178ec88530
Dylan Tang <dtang04@uchicago.edu>
2023-06-02 08:14:13 -0500

    Updated mechanism for rehashing

--- hash_test.py
+++ hash_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 class Node:
     def __init__(self, key):
         self.key = key
@@ -15,13 +16,18 @@ class HashTable:
         for i in population:
             self.table[i] = None
         
-    def insert(self, samp_id, key):
+    def insert(self, samp_id, key, numcycles = 0):
         flag = False
         current = self.table[key]
         if current == None:
             flag = True
         prev = None
         while (current != None):
+            if (current.key == samp_id and numcycles < 100):
+                self.insert(samp_id, np.random.choice(population), numcycles+1) #rehash if the sample already has the key value
+                return
+            elif (current.key == samp_id and numcycles >= 100):
+                raise Exception("Too many rehashes.")
             prev = current
             current = current.next
         current = Node(samp_id)
@@ -32,7 +38,7 @@ class HashTable:
         self.numelements += 1
 
     def load_factor(self):
-        print("Load Factor: \n" + str(self.numelements / self.n_buckets))
+        print("Load Factor: " + str(self.numelements / self.n_buckets))
 
     def print_hash(self):
         print("-------------------------")
@@ -56,7 +62,7 @@ results = HashTable()
 results.init_table(population)
 for samp_id in range(1,nsamples+1):
     choices = np.random.choice(population, samplesize)
-    print(choices)
+    print("Selected buckets: " + str(choices))
     for i in choices:
         results.insert(samp_id, i)
 results.print_hash()
